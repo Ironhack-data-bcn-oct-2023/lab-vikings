516647d93aea0e754163dae9e871b2eff2067c5d
patricia.saez <p.saeztorres@gmail.com>
2023-10-14 17:58:00 +0200

    commit with 8 errors in war part because war 'War' object has no attribute 'strength'

--- vikingsClasses.py
+++ vikingsClasses.py
@@ -2,23 +2,86 @@
 # Soldier
 
 
-class Soldier:
-    pass
+class Soldier ():
+    def __init__(self, health,strength):
+        self.health = health
+        self.strength = strength
+    
+    def attack (self):
+        return self.strength
+    
+    def receiveDamage (self,damage):
+        self.health = self.health - damage
+        
+
+
 
 # Viking
 
 
-class Viking:
-    pass
+class Viking (Soldier):
+    def __init__(self, name, health, strength):
+        super().__init__(health, strength)
+        self.name = name
+
+    def receiveDamage(self, damage):
+        self.health -= damage
+        if self.health > 0:
+            return f"{self.name} has received {damage} points of damage"
+        else:
+            return f"{self.name} has died in act of combat"
+    
+    def battleCry (self):
+        return f"Odin Owns You All!"
+
 
 # Saxon
 
 
-class Saxon:
-    pass
+class Saxon(Soldier):
+    def __init__(self, health, strength):
+        super().__init__(health, strength)
+    
+    def receiveDamage(self, damage):
+        self.health -= damage
+        if self.health > 0:
+            return f"A Saxon has received {damage} points of damage"
+        else:
+            return f"A Saxon has died in combat"   
+
 
 # War
 
 
-class War:
-    pass
+class War():
+    def __init__(self):
+    
+        self.vikingArmy = []
+        self.saxonArmy = []
+    
+
+    def addViking(self, viking):
+        self.vikingArmy.append(viking)
+    
+    def addSaxon(self,saxon):
+        self.saxonArmy.append(saxon)
+
+    def vikingAttack(self):
+        a= Saxon.receiveDamage(Viking.attack(self))
+        self.saxonArmy=[i for i in self.saxonArmy if Saxon.health > 0]
+        return a , Viking.attack(self)
+    
+
+    def saxonAttack(self):
+        b = Viking.receiveDamage(Saxon.attack(self))
+        self.vikingArmy=[i for i in self.vikingArmy if Viking.health > 0]
+        return b , Saxon.attack(self)
+    
+    def showStatus(self):
+        if len(self.saxonArmy) == 0:
+            return "Vikings have won the war of the century!"
+        elif len (self.vikingArmy) == 0:
+            return "Saxons have fought for their lives and survive another day..."
+        else:
+            return "Vikings and Saxons are still in the thick of battle."
+
